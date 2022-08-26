7e1f7eb53ac3e94bc54550ccdb4db80139174a0a
erfansolooki <erfansolookideveloper@gmail.com>
2022-08-26 15:07:43 +0430

    style filter component

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import SelectComponent from "../../Common/SelectComponent";
 import { useProductsDispatcher } from "../../Context/ProductsProvider";
 import "./Filter.css";
 
@@ -16,50 +15,51 @@ const Filter = () => {
   }, []);
 
   const activeButtonHandler = (event) => {
-    const buttonId = event.target.id;
-    setActive(buttonId);
+    setActive(event.target.id);
     dispatcher({
       type: "SORT_BY",
-      payload: active,
+      payload: event.target.id,
     });
   };
 
   return (
-    <section dir="rtl" className="sortButtons pt-3">
-      <button
-        key={4}
-        className={active === "4" ? "sortActiveButton" : undefined}
-        id={"4"}
-        onClick={activeButtonHandler}
-      >
-        جدیدترین
-      </button>
-      <button
-        key={1}
-        className={active === "1" ? "sortActiveButton" : undefined}
-        id={"1"}
-        onClick={activeButtonHandler}
-      >
-        بیشترین قیمت
-      </button>
-      {/* <span className="mt-2"> */}
-      <button
-        key={2}
-        className={active === "2" ? "sortActiveButton" : undefined}
-        id={"2"}
-        onClick={activeButtonHandler}
-      >
-        کم ترین قیمت
-      </button>
-      <button
-        key={3}
-        className={active === "3" ? "sortActiveButton" : undefined}
-        id={"3"}
-        onClick={activeButtonHandler}
-      >
-        پروفروش ترین
-      </button>
-      {/* </span> */}
+    <section dir="rtl" className="sortButtons d-flex align-items-center pt-3">
+      <p className=" me-2 ">مرتب سازی بر اساس :</p>
+      <div className="buttons d-flex">
+        <button
+          key={4}
+          className={active === "4" ? "sortActiveButton" : undefined}
+          id={"4"}
+          onClick={activeButtonHandler}
+        >
+          جدیدترین
+        </button>
+        <button
+          key={1}
+          className={active === "1" ? "sortActiveButton" : undefined}
+          id={"1"}
+          onClick={activeButtonHandler}
+        >
+          بیشترین قیمت
+        </button>
+
+        <button
+          key={2}
+          className={active === "2" ? "sortActiveButton" : undefined}
+          id={"2"}
+          onClick={activeButtonHandler}
+        >
+          کمترین قیمت
+        </button>
+        <button
+          key={3}
+          className={active === "3" ? "sortActiveButton" : undefined}
+          id={"3"}
+          onClick={activeButtonHandler}
+        >
+          پروفروش ترین
+        </button>
+      </div>
     </section>
   );
 };
